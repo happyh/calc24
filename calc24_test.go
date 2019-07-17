0d9a927171a12a7abc1180123dc278236cea19cc
happyhe <root@ops-13f734f06.qiyi.virtual>
2019-07-17 14:40:45 +0800

    名字从count24修改为calc24

--- calc24_test.go
+++ calc24_test.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func Test_main(t *testing.T) {
+func Test_dsf(t *testing.T) {
 	num := []float64{3, 3, 8, 8}
 	str := []string{"3", "3", "8", "8"}
 
 	if !dsf2(num, str, false, 24.0) {
 		t.Error("failed@", num)
 	}
+}
+
+func Test_divzero(t *testing.T) {
+	a := 1.0
+	b := 0.0
+	c := a / b
+	fmt.Println("c:", c)
 }
